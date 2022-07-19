2dab4de6bd80bd5f7e87470315de7c0013fa648d
Emanuel Veronez <emanuel.veronezz@gmail.com>
2022-07-18 23:32:39 -0300

    modified:   script.js

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ $(document).ready(function() {
         margin: 20,
         loop: true,
         autoplay: true,
-        autoplayTimeout: 1800,
+        autoplayTimeout: 2000,
         autoplayHoverPause: true,
 
         responsive: {
